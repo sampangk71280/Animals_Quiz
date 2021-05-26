1b821159b309136a7d530fb8cc9acd446e3daf9b
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2021-05-27 11:53:50 +1200

    CSV finished

--- 03_CSV_v2.py
+++ 03_CSV_v2.py
@@ -3,18 +3,23 @@ import csv
 import random
 
 results = []
-with open('animals_young_quiz.csv') as f:
+with open('animals_young_quiz.csv') as file:
 
     # make csv file into a list
-    file = csv.reader(f)
-    next(f)
-    my_list = list(file)
+    reader = csv.reader(file)
+    next(reader)
+    for row in reader:
+        results.append(row)
 
-question_ans = random.choice(my_list)
-print(question_ans)
+question_list = random.choice(results) # randomly chooses a row
+print(question_list)
 
-question = question_ans[0]
-answer = question_ans[1]
+question = question_list[0] # adult animal term
+answer = question_list[1] # baby animal term
+print("The baby term for a {} is a {}".format(question, answer)) # for testing purposes
+
+
+
 
 
 
