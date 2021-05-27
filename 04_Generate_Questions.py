92a49a9451684f04ac46765f622c7f390dd44909
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2021-05-28 10:55:49 +1200

    needs to continue working on generate questions

--- 04_Generate_Questions.py
+++ 04_Generate_Questions.py
@@ -5,6 +5,25 @@ import random
 def input_checker():
     ask = input("What is the baby term for a {}?".format(question).lower())
 
+    if ask == answer:
+        print("Good job!")
+
+
+results = []
+with open('animals_quiz(final).csv') as file:
+
+    # make csv file into a list
+    reader = csv.reader(file)
+    next(reader)
+    for row in reader:
+        results.append(row)
+
+question_list = random.choice(results) # randomly chooses a row
+print(question_list)
+
+question = question_list[0] # adult animal term
+answer = question_list[1] # baby animal term
+ask = input_checker()
 
 
 
